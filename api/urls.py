c9f70f5b9948246157fd8667180728ebd3fd9e83
Sebastian Tenorio <joan.tenorio@uisek.edu.ec>
2026-06-15 18:07:48 -0500

    Trabajo oauth terminado

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import PokemonViewSet
+from .views import PokemonViewSet, TrainerViewSet
 
 router = routers.DefaultRouter()
 router.register(r'pokemons', PokemonViewSet)
+router.register(r'trainers', TrainerViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- api/views.py
+++ api/views.py
@@ -1,11 +1,17 @@
 from rest_framework import viewsets
-from pokedex.models import Pokemon
-from .serializers import PokemonSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from oauth2_provider.contrib.rest_framework import OAuth2Authentication
+from pokedex.models import Pokemon, Trainer
+from .serializers import PokemonSerializer, TrainerSerializer
 
 class PokemonViewSet(viewsets.ModelViewSet):
-
     queryset = Pokemon.objects.all()
-
     serializer_class = PokemonSerializer
+    authentication_classes = [OAuth2Authentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
-
+class TrainerViewSet(viewsets.ModelViewSet):
+    queryset = Trainer.objects.all()
+    serializer_class = TrainerSerializer
+    authentication_classes = [OAuth2Authentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
